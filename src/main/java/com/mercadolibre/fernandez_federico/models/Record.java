799912dcc6c52b5b8bb3ec2e819ab78c720008d1
= <=>
2021-04-21 15:40:01 -0300

    Added classes and env vas

---
+++ src/main/java/com/mercadolibre/fernandez_federico/models/Record.java
@@ -0,0 +1,34 @@
+package com.mercadolibre.fernandez_federico.models;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.persistence.Column;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+public class Record {
+    @Column(nullable = false)
+    @NotNull(message = "discountType no puede ser Nulo")
+    @Size(min = 3, max=3, message = "discountType debe tener 3 caracteres")
+    @Pattern(regexp="^[a-zA-Z]{1}\\d{2}$",message="discountType debe tener 1 caracter alfabetico y 2 caracteres numericos")
+    private String discountType;
+
+    @Column(nullable = false)
+    @NotNull(message = "normalPrice no puede ser Nulo")
+    @Size(min = 4, max=9, message = "normalPrice debe tener 3 caracteres")
+    @Pattern(regexp="^(\\d{1,6})+(\\.\\d{1,2})$",message= "normalPrice debe tener 6 caracteres numericos y 2 decimales")
+    private Double normalPrice;
+
+    @Column(nullable = false)
+    @NotNull(message = "urgentPrice no puede ser Nulo")
+    @Size(min = 4, max=9, message = "urgentPrice debe tener 3 caracteres")
+    @Pattern(regexp="^(\\d{1,6})+(\\.\\d{1,2})$",message= "urgentPrice debe tener 6 caracteres numericos y 2 decimales")
+    private Double urgentPrice;
+
+    @Column(nullable = false)
+    @NotNull(message = "lastModification no puede ser Nulo")
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private Date lastModification;
+}
