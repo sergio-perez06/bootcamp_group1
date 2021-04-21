391f008e2e1c18408a3ad6e757e4afbf315fc547
sergio-perez06 <sergio.perez@mercadolibre.com.co>
2021-04-21 14:48:08 -0500

    Subsidiary

--- src/main/java/com/mercadolibre/fernandez_federico/models/Subsidiary.java
+++ src/main/java/com/mercadolibre/fernandez_federico/models/Subsidiary.java
@@ -1,4 +1,36 @@
 package com.mercadolibre.fernandez_federico.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@Table(name = "SUBSIDIARY")
+@Entity
+@Getter
+@Setter
 public class Subsidiary {
+    @Column(name = "ID_SUBSIDIARY", nullable = false)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idSubsidiary;
+
+
+    @Column(nullable = false)
+    @NotNull(message = "name no puede ser Nulo.")
+    @Size(min = 2, max=100, message = "name debe tener entre 2 y 100 caracteres.")
+    @Pattern(regexp="^[a-zA-Z]+$",message="name debe tener letras.")
+    private String name;
+
+
+    //private List<Order> orders;
+
+    @ManyToOne
+    @JoinColumn(name="idCountryDealer", nullable = false)
+    private CountryDealer countryDealer;
+
+
 }
