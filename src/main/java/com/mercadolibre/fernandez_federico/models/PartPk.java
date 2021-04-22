c78684fa421460688dd1e2254ac0dd2d67d78ac2
= <=>
2021-04-22 12:06:59 -0300

    Fix on part embed

--- src/main/java/com/mercadolibre/fernandez_federico/models/PartPk.java
+++ src/main/java/com/mercadolibre/fernandez_federico/models/PartPk.java
@@ -3,12 +3,11 @@ package com.mercadolibre.fernandez_federico.models;
 import javax.persistence.Embeddable;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
 import java.io.Serializable;
 
 @Embeddable
 public class PartPk implements Serializable {
     @ManyToOne
-    @JoinColumn(name = "idPart", referencedColumnName = "idPart", nullable = false)
-    private Part idPart;
+    @JoinColumn(name = "idPart", nullable = false)
+    private Part part;
 }
