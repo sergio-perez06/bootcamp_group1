4c85938a8259304e4dcb86342d8bfdfc3e28dbbb
christianguantay <christian.guantay@mercadolibre.com>
2021-04-22 11:34:45 -0300

    Agrego Maker y Stock Subsidiary

--- src/main/java/com/mercadolibre/fernandez_federico/models/StockSubsidiary.java
+++ src/main/java/com/mercadolibre/fernandez_federico/models/StockSubsidiary.java
@@ -1,4 +1,36 @@
 package com.mercadolibre.fernandez_federico.models;
 
+
+import lombok.Getter;
+import lombok.Setter;
+import org.checkerframework.common.aliasing.qual.Unique;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@Entity
+@Getter
+@Setter
+@Table(name="STOCKSUBSIDIARY")
 public class StockSubsidiary {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idStockSubsidiary;
+
+    @Unique
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name="idPart", referencedColumnName = "idPart", nullable = false)
+    private Part part;
+
+    @Unique
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name="idSubsidiary", referencedColumnName = "idSubsidiary", nullable = false)
+    private Subsidiary subsidiary;
+
+    @Column(nullable = false)
+    @NotNull(message = "El tamaño no puede ser nulo")
+    private Long Quantity;
+
 }
