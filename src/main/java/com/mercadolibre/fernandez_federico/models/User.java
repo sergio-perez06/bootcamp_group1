ae82d538f18a29e745aa3e7327ea002a85cee7de
María Micaela Sejas <micaela.sejas@mercadolibre.com>
2021-04-22 12:48:42 -0300

    Changes in Oser, Order, StockSubsidiary,StockService

--- src/main/java/com/mercadolibre/fernandez_federico/models/User.java
+++ src/main/java/com/mercadolibre/fernandez_federico/models/User.java
@@ -29,8 +29,8 @@ public class User
     @Pattern(regexp = "^[a-zA-Z\\d]{5,12}", message = "username debe tener entre cinco y doce caracteres alfanumericos")
     private String password;
 
-    @OneToMany(mappedBy = "user")
-    private List<Order> orders;
+//    @OneToMany(mappedBy = "user")
+//    private List<Order> orders;
 
     @ManyToOne
     @JoinColumn(name="idUserRol", nullable = false)
