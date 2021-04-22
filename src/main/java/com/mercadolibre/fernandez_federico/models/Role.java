f28302ee41a6e33cdee7406bbd8ad8abfc5f42c6
María Micaela Sejas <micaela.sejas@mercadolibre.com>
2021-04-22 14:15:28 -0300

    Merge

---
+++ src/main/java/com/mercadolibre/fernandez_federico/models/Role.java
@@ -0,0 +1,21 @@
+package com.mercadolibre.fernandez_federico.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+
+@Table(name="role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idRol;
+
+    private String name;
+
+}
