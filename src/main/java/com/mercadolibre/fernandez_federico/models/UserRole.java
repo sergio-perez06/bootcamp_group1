c3a001840dc11675eed5f6bc4fe9d600554519d7
María Micaela Sejas <micaela.sejas@mercadolibre.com>
2021-04-22 11:20:23 -0300

    Changes in roles

--- src/main/java/com/mercadolibre/fernandez_federico/models/UserRole.java
+++ src/main/java/com/mercadolibre/fernandez_federico/models/UserRole.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class UserRole {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long idUserRol;
+    private long idUserRole;
     private String name;
     @OneToMany(mappedBy ="user_role" )
     private List<User> users;
