0c02dc2796b727a1c1776893d18ea934ca0f28df
sergio.perez <sergio.perez@mercadolibre.com.co>
2021-04-26 10:45:24 -0500

    remove unused dependencies on PingController

--- src/main/java/com/mercadolibre/fernandez_federico/controller/PingController.java
+++ src/main/java/com/mercadolibre/fernandez_federico/controller/PingController.java
@@ -1,19 +1,11 @@
 package com.mercadolibre.fernandez_federico.controller;
 
 import com.mercadolibre.fernandez_federico.util.TokenUtils;
-import com.mercadolibre.fernandez_federico.util.filters.AuthenticationFilter;
 import com.newrelic.api.agent.NewRelic;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwt;
-import io.jsonwebtoken.Jwts;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.xml.bind.DatatypeConverter;
-import java.util.Hashtable;
 import java.util.Map;
 
 @RestController
