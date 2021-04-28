a42d9c698b7d72a8913d80e7edc3c7b70db98c85
Guillermo Biatturi <guillermo.biatturi@mercadolibre.com>
2021-04-28 10:42:33 -0300

    Fix push - Removed content of stockwarehouse svc

--- src/main/java/com/mercadolibre/fernandez_federico/services/impl/StockWarehouseService.java
+++ src/main/java/com/mercadolibre/fernandez_federico/services/impl/StockWarehouseService.java
@@ -2,11 +2,14 @@ package com.mercadolibre.fernandez_federico.services.impl;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.stream.Collectors;
 
+import java.util.Comparator;
+
 import com.mercadolibre.fernandez_federico.dtos.request.CountryDealerStockDTO;
-import com.mercadolibre.fernandez_federico.dtos.responses.BillDTO;
 import com.mercadolibre.fernandez_federico.dtos.responses.CountryDealerStockResponseDTO;
 import com.mercadolibre.fernandez_federico.dtos.responses.PartDTO;
 import com.mercadolibre.fernandez_federico.dtos.responses.SubsidiaryOrdersByDeliveryStatusDTO;
@@ -16,6 +19,7 @@ import com.mercadolibre.fernandez_federico.repositories.*;
 import com.mercadolibre.fernandez_federico.services.IStockWarehouseService;
 import org.modelmapper.ModelMapper;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -28,39 +32,33 @@ public class StockWarehouseService implements IStockWarehouseService {
     private IMakerRepository makerRepository;
     private IRecordRepository recordRepository;
     private ModelMapper modelMapper;
-    private IStockCountryDealerRepository stockCountryDealerRepository;
-    private IPartRepository partRepository;
+    private IUserRepository userRepository;
 
     public StockWarehouseService(IStockWarehouseRepository stockWarehouseRepository, ModelMapper modelMapper, IDiscountTypeRepository discountTypeRepository,
                                  ICountryDealerRepository countryDealerRepository, ISubsidiaryRepository subsidiaryRepository,IMakerRepository makerRepository,
-                                 IRecordRepository recordRepository, IStockCountryDealerRepository stockCountryDealerRepository, IPartRepository partRepository)
+                                 IRecordRepository recordRepository)
     {
         this.stockWarehouseRepository = stockWarehouseRepository;
         this.discountTypeRepository = discountTypeRepository;
         this.makerRepository = makerRepository;
         this.recordRepository = recordRepository;
         this.modelMapper = modelMapper;
-        this.countryDealerRepository = countryDealerRepository;
-        this.subsidiaryRepository = subsidiaryRepository;
-        this.stockCountryDealerRepository = stockCountryDealerRepository;
-        this.partRepository = partRepository;
     }
 
     @Override
     public List<PartDTO> getParts(HashMap<String, String> filters) throws Exception {
-        if (stockWarehouseRepository.findAll().isEmpty())
-            throw new ApiException("Not Found","La lista no existe",404 );
-        else
-        {
+        if(stockWarehouseRepository.findAll().isEmpty())
+            throw new ApiException(HttpStatus.NOT_FOUND.name(), "La lista no existe.", HttpStatus.NOT_FOUND.value());
+        else {
             //Se cargan los repositorios por separado trayendo lista entera
               List<PartDTO> partsDTO = new ArrayList<>();
               List<StockWarehouse> stockWarehouses = stockWarehouseRepository.findAll()
                       .stream()
                       .map(stock -> modelMapper.map(stock, StockWarehouse.class))
                       .collect(Collectors.toList());
-            List<Maker> maker = makerRepository.findAll()
+            List<Maker> makers = makerRepository.findAll()
                     .stream()
-                    .map(maker1 -> modelMapper.map(maker1, Maker.class))
+                    .map(maker -> modelMapper.map(maker, Maker.class))
                     .collect(Collectors.toList());
             List<DiscountType> discountTypes = discountTypeRepository.findAll()
                     .stream()
@@ -70,17 +68,11 @@ public class StockWarehouseService implements IStockWarehouseService {
                     .stream()
                     .map(record -> modelMapper.map(record, Record.class))
                     .collect(Collectors.toList());
-
+            //toDo: La función de abajo podría reformularse para no volverla a hacer en todos los ifs y que en los filtros
+            // p y v elimine filas.
             if (filters.isEmpty() || (filters.get("queryType").equals("C"))) {
                 for(int i=0; i<stockWarehouses.size(); i++){
-                    PartDTO part = new PartDTO();
-                    part.setPartCode(stockWarehouses.get(i).getPart().getPartCode());
-                    part.setDescription(stockWarehouses.get(i).getPart().getDescription());
-                    part.setNetWeight(stockWarehouses.get(i).getPart().getNetWeight());
-                    part.setLongDimension(stockWarehouses.get(i).getPart().getLongDimension());
-                    part.setWidthDimension(stockWarehouses.get(i).getPart().getWidthDimension());
-                    part.setTallDimension(stockWarehouses.get(i).getPart().getTallDimension());
-                    part.setQuantity(stockWarehouses.get(i).getQuantity());
+                    PartDTO part = modelMapper.map(stockWarehouses.get(i), PartDTO.class);
                     for(int f=0; f<records.size(); f++){
                         if(stockWarehouses.get(i).getPart().getId().equals(records.get(f).getPart().getId())){
                             part.setNormalPrice(records.get(f).getNormalPrice());
@@ -93,42 +85,35 @@ public class StockWarehouseService implements IStockWarehouseService {
                             }
                         }
                     }
-                    for(int f=0; f<maker.size();f++){
-                        if(stockWarehouses.get(i).getPart().getMaker().getId().equals(maker.get(f).getId())){
-                            part.setMaker(maker.get(f).getName());
+                    for(int f=0; f<makers.size();f++){
+                        if(stockWarehouses.get(i).getPart().getMaker().getId().equals(makers.get(f).getId())){
+                            part.setMaker(makers.get(f).getName());
                         }
                     }
                     partsDTO.add(part);
                 }
-            }
+                // return partsDTO;
 
-            if (filters.containsKey("queryType") && (filters.get("queryType").equals("P") && filters.containsKey("date")))
+            }if (filters.containsKey("queryType") && (filters.get("queryType").equals("P") && filters.containsKey("date")))
             {
                 for(int f=0; f<records.size(); f++){
-
                     for(int i=0; i<stockWarehouses.size(); i++){
                         PartDTO part = new PartDTO();
                         //Se filtra por fecha y luego se hace la carga
                         if(records.get(f).getLastModification().compareTo(LocalDate.parse(filters.get("date"),DateTimeFormatter.ofPattern("yyyy-MM-dd")))>=0){
                             if(stockWarehouses.get(i).getPart().getId().equals(records.get(f).getPart().getId())){
+                                part = modelMapper.map(stockWarehouses.get(i), PartDTO.class);
                                 part.setNormalPrice(records.get(f).getNormalPrice());
                                 part.setUrgentPrice(records.get(f).getUrgentPrice());
                                 part.setLastModification(records.get(f).getLastModification().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
-                                part.setPartCode(stockWarehouses.get(i).getPart().getPartCode());
-                                part.setDescription(stockWarehouses.get(i).getPart().getDescription());
-                                part.setNetWeight(stockWarehouses.get(i).getPart().getNetWeight());
-                                part.setLongDimension(stockWarehouses.get(i).getPart().getLongDimension());
-                                part.setWidthDimension(stockWarehouses.get(i).getPart().getWidthDimension());
-                                part.setTallDimension(stockWarehouses.get(i).getPart().getTallDimension());
-                                part.setQuantity(stockWarehouses.get(i).getQuantity());
                                 for (int g = 0; g < discountTypes.size(); g++) {
                                     if (records.get(f).getDiscountType().getId().equals(discountTypes.get(g).getId())) {
                                         part.setDiscountType(discountTypes.get(g).getType());
                                     }
                                 }
-                                for (int g = 0; g < maker.size(); g++) {
-                                    if (stockWarehouses.get(i).getPart().getMaker().getId().equals(maker.get(g).getId())) {
-                                        part.setMaker(maker.get(g).getName());
+                                for (int g = 0; g < makers.size(); g++) {
+                                    if (stockWarehouses.get(i).getPart().getMaker().getId().equals(makers.get(g).getId())) {
+                                        part.setMaker(makers.get(g).getName());
                                     }
                                 }
                             partsDTO.add(part);
@@ -139,6 +124,10 @@ public class StockWarehouseService implements IStockWarehouseService {
                     }
 
                 }
+
+                //return partsDTO;
+
+                //toDO: aún no hicimos el tercer filtro, se hará en el transcurso del finde o principios de lunes.
             } if (filters.containsKey("queryType") && (filters.get("queryType").equals("V") && filters.containsKey("date"))){
                 HashMap<Long, Double> partHashMap = new HashMap<>();
                 for(int f=0; f<records.size(); f++){
@@ -150,9 +139,13 @@ public class StockWarehouseService implements IStockWarehouseService {
                                 if(!partHashMap.containsKey(stockWarehouses.get(i).getPart().getId())){
                                     partHashMap.put(stockWarehouses.get(i).getPart().getId(),records.get(f).getNormalPrice());
                                 }
+
                             }
+
                         }
+
                     }
+
                 }
                 for(int f=0; f<records.size(); f++) {
                     PartDTO part = new PartDTO();
@@ -168,6 +161,7 @@ public class StockWarehouseService implements IStockWarehouseService {
                         }
                     }
                 }
+
             }if(filters.containsKey("order")){
                 if(filters.get("order").equals("1")){
                     partsDTO.sort(Comparator.comparing(PartDTO::getDescription));}
