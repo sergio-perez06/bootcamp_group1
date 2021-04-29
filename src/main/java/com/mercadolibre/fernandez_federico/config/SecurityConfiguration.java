16a7e84925efd1ed30406bcb4f58f45440c24283
Guillermo Biatturi <guillermo.biatturi@mercadolibre.com>
2021-04-29 17:16:57 -0300

    Edit /ping doesnt require Auth token

--- src/main/java/com/mercadolibre/fernandez_federico/config/SecurityConfiguration.java
+++ src/main/java/com/mercadolibre/fernandez_federico/config/SecurityConfiguration.java
@@ -15,6 +15,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import static com.mercadolibre.fernandez_federico.util.SecurityConstants.PING_URL;
 import static com.mercadolibre.fernandez_federico.util.SecurityConstants.SIGN_UP_URL;
 
 @EnableWebSecurity
@@ -32,6 +33,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().authorizeRequests()
                 .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
+                .antMatchers(HttpMethod.POST, PING_URL).permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .addFilter(new AuthenticationFilter(authenticationManager(),userService))
