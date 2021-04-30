6946d6340673479995884254b4de55ebbead4933
Guillermo Biatturi <guillermo.biatturi@mercadolibre.com>
2021-04-30 17:12:31 -0300

    Refactor REQ 3 (getBillDetails)

--- src/main/java/com/mercadolibre/fernandez_federico/services/impl/BillService.java
+++ src/main/java/com/mercadolibre/fernandez_federico/services/impl/BillService.java
@@ -9,10 +9,13 @@ import com.mercadolibre.fernandez_federico.models.BillDetail;
 import com.mercadolibre.fernandez_federico.repositories.*;
 import com.mercadolibre.fernandez_federico.services.IBillService;
 import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 
 @Service
@@ -21,38 +24,40 @@ public class BillService implements IBillService {
     private IBillDetailRepository billDetailRepository;
     private ModelMapper modelMapper;
 
-    public BillService(IBillRepository billRepository, IBillDetailRepository billDetailRepository, ModelMapper modelMapper)
-    {
+    public BillService(IBillRepository billRepository, IBillDetailRepository billDetailRepository, ModelMapper modelMapper) {
         this.billRepository = billRepository;
         this.billDetailRepository = billDetailRepository;
         this.modelMapper = modelMapper;
     }
 
-    public BillDTO getBillDetails(String orderNumberCM){
+    public BillDTO getBillDetails(String orderNumberCM) {
         BillDTO finalBill = new BillDTO();
-        if(billRepository.findAll().isEmpty())
-            throw new ApiException("Not Found", "La orden no existe", 404);
-        else{
+
+        if (billRepository.findAll().isEmpty()) {
+            throw new ApiException(NOT_FOUND.name(), "La orden no existe", NOT_FOUND.value());
+        } else {
             Bill bills = billRepository.findByCmOrdernumberWarehouse(orderNumberCM);
             List<BillDetail> billsDetail = billDetailRepository.findAll();
-                if(bills.getCmOrdernumberWarehouse().equals(orderNumberCM)){
-                    String[] ord =bills.getCmOrdernumberWarehouse().split("-");
-                    finalBill.setOrderNumber(ord[1]+"-"+ord[2]);
-                    finalBill.setOrderDate(bills.getOrderDate());
-                    finalBill.setDeliveryStatus(bills.getDeliveryStatus());
-                    List<BillDetailDTO> billDetailDTOS = new ArrayList<>();
-                    BillDetailDTO billDetailDTO = new BillDetailDTO();
-                    for(int i=0; i<billsDetail.size(); i++){
-                        if(bills.getId().equals(billsDetail.get(i).getBill().getId())){
-                            billDetailDTO = modelMapper.map(billsDetail.get(i), BillDetailDTO.class);
-                            billDetailDTOS.add(billDetailDTO);
-                        }
+
+            if (bills.getCmOrdernumberWarehouse().equals(orderNumberCM)) {
+                String[] ord = bills.getCmOrdernumberWarehouse().split("-");
+                finalBill.setOrderNumber(ord[1] + "-" + ord[2]);
+                finalBill.setOrderDate(bills.getOrderDate());
+                finalBill.setDeliveryStatus(bills.getDeliveryStatus());
+
+                List<BillDetailDTO> billDetailDTOS = new ArrayList<>();
+
+                billsDetail.forEach(billDetail -> {
+                    if (bills.getId().equals(billDetail.getBill().getId())) {
+                        BillDetailDTO billDetailDTO = modelMapper.map(billDetail, BillDetailDTO.class);
+                        billDetailDTOS.add(billDetailDTO);
                     }
-                    finalBill.setOrderDetails(billDetailDTOS);
-                }
+                });
+
+                finalBill.setOrderDetails(billDetailDTOS);
             }
-
-
+        }
+        
         return finalBill;
     }
 }
